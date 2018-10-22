181bc9215079b8d04dc1a543c5a162784d6e09e0
Michael <Jahnsmic001@gmail.com>
2018-10-22 14:05:39 -0700

    rendering to page

--- app/app.js
+++ app/app.js
@@ -1,38 +1,47 @@
 var cardDeck = [];
 var allProducts = [];
+var window1 = document.getElementById('test1');
+var window2 = document.getElementById('test2');
+var window3 = document.getElementById('test3');
 
-function product (name) {
-    this.name= name;
+
+function product (name, description) {
+    this.name = name;
+    this.description = description;
     this.views = 0;
     this.clicks = 0;
 
     allProducts.push(this); 
 }
 
-new product (' 1');
-new product (' 2');
-new product (' 3');
-new product (' 4');
-new product (' 5');
-new product (' 6');
-console.table(allProducts);
+new product (' 1', 'one for the money');
+new product (' 2', 'two for the show');
+new product (' 3', 'three to get ready');
+new product (' 4', 'four to go');
+new product (' 5', 'five to do other stuff');
+new product (' 6', 'six was there too');
 
-function shuffle(array)
-{
+function shuffle(array){
+    console.table(allProducts);
     cardDeck = array.slice(0);
-    console.log(cardDeck)
-
     for (var i = cardDeck.length - 1; i > 0; i--) {
-        //make randomPosition = random number inclusive
         var randomPosition = Math.floor(Math.random() * (i + 1));
-        //grab object at current array idx and store
         var temp = cardDeck[i];
-        //overrides array idx with random number object
         cardDeck[i] = cardDeck[randomPosition];
-        //places overridden arrady idx on randomPositions old value.
         cardDeck[randomPosition] = temp;
     }
-    console.table(allProducts)
     console.table(cardDeck)
 }
-shuffle();
+shuffle(allProducts);
+
+function createElement(type, content , parent){
+    var element = document.createElement(type);
+    element.textContent = content;
+    parent.appendChild(element);
+}
+
+function render(){
+createElement('p', cardDeck[0].description, window1);
+createElement('p', cardDeck[1].description, window2);
+createElement('p', cardDeck[2].description, window3);
+};
